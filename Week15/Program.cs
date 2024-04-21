a31f5e0a07dc3c1e7da2373d9fcc0c476a70b1dd
Tanel Ristsalu <tanel.ristsalu@tktk.ee>
2024-04-21 15:47:38 +0300

    Add project files.

---
+++ Week15/Program.cs
@@ -0,0 +1,55 @@
+﻿
+
+List<Film> minuFilmid = new List<Film>();
+string[] data = AndmedFailist();
+//loeAndmed(data);
+
+foreach (string line in data)
+{
+    string[] tempMassiiv = line.Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries);
+    Film uusFilm = new Film(tempMassiiv[0], tempMassiiv[2]);
+    minuFilmid.Add(uusFilm);
+}
+
+foreach (Film film in minuFilmid)
+{
+    Console.WriteLine($"Minu lemmikfilm {film.Pealkiri} tuli välja aastal {film.Aasta}.");
+}
+
+static string[] AndmedFailist()
+{
+    string asukoht = @"C:\Users\rists\Desktop\TKTK KMT2023\1-K Programmeerimise algkursus - kevad 2024 - J. Voronetskaja\Week15\movies.txt";
+    string[] andmed = File.ReadAllLines(asukoht);
+
+    return andmed;
+}
+
+static void loeAndmed(string[] massiiv)
+{
+    foreach(string line in massiiv)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+
+class Film
+{
+    string pealkiri;
+    string aasta;
+
+    public string Pealkiri
+    {
+        get { return pealkiri; }
+    }
+    public string Aasta
+    {
+        get { return aasta; }
+    }
+
+    public Film(string _pealkiri, string _aasta)
+    {
+        pealkiri = _pealkiri;
+        aasta = _aasta;
+    }
+}
